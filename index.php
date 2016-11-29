79f61b5787aea356ccdaf954b7d5f23cee47e11a
otake <otake.hiroyo@gmail.com>
2016-11-30 07:53:56 +0900

    配列練習問題

--- index.php
+++ index.php
@@ -1,11 +1,46 @@
 <?php
 
-$fruits = array('red'=>'りんご','yellow'=>'ばなな','green'=>'メロン');
+// 配列 連想配列 の練習問題
+//
+// // 1) 以下のような配列が与えられている。この時に、イヌと出力するには下記の???に何を入力すればよいか答えよ。
 
-echo $fruits['yellow'];
+$animals = array('イヌ', 'ネコ', 'うさぎ');
 
-$fruits['pink'] = 'peach';
+echo $animals[0];
 
-var_dump($fruits);
+// 2)
+// // 次のような連想配列が与えられている。この場合に、 卓球 と出力するには???に何を入力すればよいか答えよ。
+
+
+$sports = array(
+'サッカー' => '蹴球',
+'ベースボール' => '野球',
+'バスケットボール' => '籠球',
+'ピンポン' => '卓球'
+);
+
+echo $sports['ピンポン'];
+
+// 3) var_dump($fruits); としたとき、下記の内容が出力されるような$fruitsを定義せよ。
+
+// array(4) {
+// 	["red"] =>
+// 	string(5) "apple"
+// 	["yellow"] =>
+// 	string(6) "banana"
+// 	["green"] =>
+// 	string(5) "melon"
+// 	["purple"] =>
+// 	string(5) "grape"
+// }
+
+$fruits = array(
+	'red' => 'apple',
+	'yellow' => 'banana',
+	'green' => 'melon',
+	'purple' => 'grape'
+	);
+
+var_dump($fruits)
 
 ?>
