e37993ef8e30e2b33a94350921c0d97a907cefa3
otake <otake.hiroyo@gmail.com>
2016-11-30 08:09:51 +0900

    foreach

--- index.php
+++ index.php
@@ -1,38 +1,16 @@
 <?php
 
-// 配列 連想配列 の練習問題
-//
-// // 1) 以下のような配列が与えられている。この時に、イヌと出力するには下記の???に何を入力すればよいか答えよ。
-
 $animals = array('イヌ', 'ネコ', 'うさぎ');
 
-echo $animals[0];
-
-// 2)
-// // 次のような連想配列が与えられている。この場合に、 卓球 と出力するには???に何を入力すればよいか答えよ。
-
-
-$sports = array(
-'サッカー' => '蹴球',
-'ベースボール' => '野球',
-'バスケットボール' => '籠球',
-'ピンポン' => '卓球'
-);
-
-echo $sports['ピンポン'];
-
-// 3) var_dump($fruits); としたとき、下記の内容が出力されるような$fruitsを定義せよ。
-
-// array(4) {
-// 	["red"] =>
-// 	string(5) "apple"
-// 	["yellow"] =>
-// 	string(6) "banana"
-// 	["green"] =>
-// 	string(5) "melon"
-// 	["purple"] =>
-// 	string(5) "grape"
+// for ($i = 0; $i <= 2; $i++)
+// {
+// 	echo $animals[$i];
 // }
+
+foreach ($animals as $animal)
+{
+	echo $animal . '<br>';
+}
 
 $fruits = array(
 	'red' => 'apple',
@@ -41,6 +19,11 @@ $fruits = array(
 	'purple' => 'grape'
 	);
 
-var_dump($fruits)
+// foreach (対象となる配列 as ラベルの名前 => 要素の名前)
+foreach ($fruits as $color =>  $fruit)
+{
+	echo $fruit . 'の色は' . $color . '<br>';
+}
+
 
 ?>
