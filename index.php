69cb1dfa0cda040807568422223fc7c50fe5d9bb
otake <otake.hiroyo@gmail.com>
2016-11-29 09:15:08 +0900

    演算子

--- index.php
+++ index.php
@@ -1,9 +1,49 @@
 <?php
 
-$name ='tanaka';
+// 代入演算子 =
+// // 右辺を左辺に代入する
+$message = 'Hello World';
 
-$age = 21;
+$x = 5;
 
-var_dump($age);
+
+// 代数演算子 + - * / %
+$x = 6;
+$y = 2;
+
+$add = $x + $y; // 足し算(addition)
+$sub = $x - $y; // 引き算(subtraction)
+$mul = $x * $y; // 掛け算(multiplication)
+$div = $x / $y; // 割り算(division)
+$rem = $x % $y; // 余り(remainder)
+
+// echo $add;
+// echo '<br>';
+//
+// echo $sub;
+// echo '<br>';
+//
+// echo $mul;
+// echo '<br>';
+//
+// echo $div;
+// echo '<br>';
+//
+// echo $rem;
+
+
+// 複合演算子 += *= -= /=
+$x += $y; //$x = $x + $y;
+
+// echo $x;
+
+
+// 単項演算子
+$i = 1;
+
+$i++; //$i = $i + 1;
+
+// echo $i;
+
 
 ?>
