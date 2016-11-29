c9044489b6738e47bab691868ef1f87ef56fde2c
otake <otake.hiroyo@gmail.com>
2016-11-29 19:40:35 +0900

    if_1

--- index.php
+++ index.php
@@ -1,23 +1,12 @@
 <?php
 
-// 練習問題
-// (1) $familyName $firstName $age にそれぞれ自分の姓、名、年齢を代入せよ。
-$familyName = 'たなか';
-$firstName = 'まきこ';
-$age = 72; 
+// 比較演算子 > < >= <= == !=
+// 論理演算子 &&(AND) ||(OR) !(NOT)
+//
+$score = 60;
+$age = 20;
 
-// (2) 上記の変数とecho を使って「◯◯の年齢は☆☆歳です」とブラウザに表示せよ。
-//     この際、文字列の連結を利用すること。
-
-echo $familyName . $firstName . 'の年齢は' . $age . '歳です';
-
-// (3) $fiveには整数型の5 $FIVEには文字列型の5 を代入し、
-//     それぞれデータ型を var_dump() で確認せよ。
-
-$five = 5;
-$FIVE = '5';
-
-var_dump($five);
-var_dump($FIVE);
-
+if ($score > 50 && $age = 18) {
+	echo '合格';
+}
 ?>
