8159d62a536cc9d152df0d0a5b983c6eb80f4ce9
otake <otake.hiroyo@gmail.com>
2016-11-29 19:35:37 +0900

    練習問題その1

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php
 
-$name = 'tanaka';
+// 練習問題
+// (1) $familyName $firstName $age にそれぞれ自分の姓、名、年齢を代入せよ。
+$familyName = 'たなか';
+$firstName = 'まきこ';
+$age = 72; 
 
-$age = 21;
+// (2) 上記の変数とecho を使って「◯◯の年齢は☆☆歳です」とブラウザに表示せよ。
+//     この際、文字列の連結を利用すること。
 
-echo $name . 'さんの年齢は' . $age . '歳です';
-echo "$name さんの年齢は $age 歳です"; //ちょっと間が空いてしまうので、上の表記がよい
+echo $familyName . $firstName . 'の年齢は' . $age . '歳です';
+
+// (3) $fiveには整数型の5 $FIVEには文字列型の5 を代入し、
+//     それぞれデータ型を var_dump() で確認せよ。
+
+$five = 5;
+$FIVE = '5';
+
+var_dump($five);
+var_dump($FIVE);
 
 ?>
