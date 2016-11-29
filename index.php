e4ba82f30ed4a954aeb6edbd7efb3ea217d9ffdd
otake <otake.hiroyo@gmail.com>
2016-11-29 19:17:23 +0900

    クオテーションとエスケープ文字

--- index.php
+++ index.php
@@ -1,49 +1,18 @@
 <?php
 
-// 代入演算子 =
-// // 右辺を左辺に代入する
-$message = 'Hello World';
+// '' と "" の違い: echoするときに違う
 
-$x = 5;
+$name = 'tanaka';
 
+// "" 変数やエスケープ文字が展開される
+echo "こんにちは $name さん";
 
-// 代数演算子 + - * / %
-$x = 6;
-$y = 2;
+// '' 展開されない
+echo 'こんにちは $name さん';
 
-$add = $x + $y; // 足し算(addition)
-$sub = $x - $y; // 引き算(subtraction)
-$mul = $x * $y; // 掛け算(multiplication)
-$div = $x / $y; // 割り算(division)
-$rem = $x % $y; // 余り(remainder)
-
-// echo $add;
-// echo '<br>';
-//
-// echo $sub;
-// echo '<br>';
-//
-// echo $mul;
-// echo '<br>';
-//
-// echo $div;
-// echo '<br>';
-//
-// echo $rem;
-
-
-// 複合演算子 += *= -= /=
-$x += $y; //$x = $x + $y;
-
-// echo $x;
-
-
-// 単項演算子
-$i = 1;
-
-$i++; //$i = $i + 1;
-
-// echo $i;
-
+// エスケープ文字
+echo'\'';
+echo "\t";
+echo "あ\nあ";
 
 ?>
